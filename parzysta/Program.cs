2e76d39657a18d39ea1ce7c8805b5f541a113f9d
Mariusz Wójcik <36dragan@gmail.com>
2020-07-28 20:43:35 +0200

    add metod getData to check if user return integer

--- parzysta/Program.cs
+++ parzysta/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,17 @@ namespace parzysta
         static void Main(string[] args)
         {
             Console.WriteLine("Podaj liczbe a powiem Ci czy jest parzysta czy nieparzysta:");
-            var number = Console.ReadLine();
+            var number = GetData();
+
+        }
+        private static int GetData()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int data))
+            {
+                throw new Exception("Podana wartość jest nieprawidłowa.");
+            }
+            return data;
         }
     }
+    
 }
